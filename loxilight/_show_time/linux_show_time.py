0fad7acef7cd904162a57d786560ff2a2d802f1d
seokhwan.kong@netlox.io <seokhwan.kong@netlox.io>
2022-03-10 13:29:34 +0900

    add test

---
+++ loxilight/_show_time/linux_show_time.py
@@ -0,0 +1,18 @@
+import textfsm
+
+traceroute = '''
+               Local time: Thu 2022-03-10 04:17:09 UTC
+           Universal time: Thu 2022-03-10 04:17:09 UTC
+                 RTC time: Thu 2022-03-10 04:17:07
+                Time zone: Etc/UTC (UTC, +0000)
+System clock synchronized: no
+              NTP service: inactive
+          RTC in local TZ: no
+'''
+
+with open('linux_show_time.template') as template:
+    fsm = textfsm.TextFSM(template)
+    result = fsm.ParseText(traceroute)
+
+print(fsm.header)
+print(result)
