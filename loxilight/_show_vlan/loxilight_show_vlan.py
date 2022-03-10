467bf43e8dcf12badd6980d753a202fd3a4225cd
seokhwan.kong@netlox.io <seokhwan.kong@netlox.io>
2022-03-10 16:04:11 +0900

    tet add show vlan

---
+++ loxilight/_show_vlan/loxilight_show_vlan.py
@@ -0,0 +1,16 @@
+import textfsm
+
+# cat /proc/uptime; hostnamectl
+
+traceroute = '''
+|   NAME    | VLAN | STATUS |    PORTS   |
+|-----------|------|--------|------------|
+| hsvlan110 |  110 | UP     | eth0, eth1 |
+'''
+
+with open('template.textfsm') as template:
+    fsm = textfsm.TextFSM(template)
+    result = fsm.ParseText(traceroute)
+
+print(fsm.header)
+print(result)
