5128d3e27a07ee3f6f21748f5ece0932ba65316d
seokhwan.kong@netlox.io <seokhwan.kong@netlox.io>
2022-03-10 17:11:24 +0900

    commit

--- loxilight/_show_vlan/loxilight_show_vlan.py
+++ loxilight/_show_vlan/loxilight_show_vlan.py
@@ -16,6 +16,7 @@ import textfsm
 
 traceroute = '''
 |   NAME    | VLAN | STATUS |    PORTS   |
+|-----------|------|--------|------------|
 | hsvlan110 |  110 | UP     | eth0, eth1 |
 | hsvlan120 |  120 | UP     | eth0, eth1 |
 '''
