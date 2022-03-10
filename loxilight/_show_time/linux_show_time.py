20189db00c10f83818422d2776ccca24a85f7895
seokhwan.kong@netlox.io <seokhwan.kong@netlox.io>
2022-03-10 13:54:43 +0900

    commit

--- loxilight/_show_time/linux_show_time.py
+++ loxilight/_show_time/linux_show_time.py
@@ -4,7 +4,7 @@ traceroute = '''
                Local time: Thu 2022-03-10 04:17:09 UTC
            Universal time: Thu 2022-03-10 04:17:09 UTC
                  RTC time: Thu 2022-03-10 04:17:07
-                Time zone: Etc/UTC (UTC, +0000)
+                Time zone: Etc/UTC 
 System clock synchronized: no
               NTP service: inactive
           RTC in local TZ: no
