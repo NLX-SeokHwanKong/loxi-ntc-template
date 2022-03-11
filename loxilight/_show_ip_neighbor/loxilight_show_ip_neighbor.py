3f42ca1df9e88776279dd118649f4c85fe35d77b
NLX-SeokHwanKong <seokhwan.kong@netlox.io>
2022-03-11 14:24:14 +0900

    commit

--- loxilight/_show_ip_neighbor/loxilight_show_ip_neighbor.py
+++ loxilight/_show_ip_neighbor/loxilight_show_ip_neighbor.py
@@ -1,10 +1,11 @@
 import textfsm
 
 # cat /proc/uptime; hostnamectl
-#| VRF | NEXT HOP | SOURCE MAC | DESTINAION MAC | INTERFACE | IS INSTALL | HW INSTALL |
-#|-----|----------|------------|----------------|-----------|------------|------------|
+# ^[|]\s+VRF\s+[|]\s+NEXT HOP\s+[|]\s+SOURCE MAC\s+[|]\s+DESTINAION MAC\s+[|]\s+INTERFACE\s+[|]\s+IS INSTALL\s+[|]\s+HW INSTALL.*$$ -> Next.NoRecord
 
 traceroute = '''
+| VRF | NEXT HOP | SOURCE MAC | DESTINAION MAC | INTERFACE | IS INSTALL | HW INSTALL |
+|-----|----------|------------|----------------|-----------|------------|------------|
 | 1   | 128.0.0.1 | 01:42:b4:fc:af:a9   | 01:42:b4:fc:af:19       | eth1      |    aaa        |     bbb       |
 | 2   | 228.0.0.1 | 02:42:b4:fc:af:a9   | 02:42:b4:fc:af:29       | eth2      |     ccc       |    ddd        |
 '''
